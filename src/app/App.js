8ccf94c9a6794dbe8ce572401dba2205074e178b
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-16 20:04:18 -0500

    added private routing for MyOrder,SupplierProfile,OrderForm,OrderDetail,BuyerProfile, and added public routing for Login, Register.

--- src/app/App.js
+++ src/app/App.js
@@ -11,23 +11,104 @@ import OrderDetail from '../components/OrderDetail/OrderDetail';
 import OrderForm from '../components/OrderForm/OrderForm';
 import Register from '../components/Register/Register';
 import SupplierProfile from '../components/SupplierProfile/SupplierProfile';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 
 fbConnection();
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/myorder', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state = {
+    authed: true,
+  };
+
   render () {
     if (firebase.apps.length) {
       console.error('Firebase Initialized!');
     };
     return (
       <div className="App">
-        <BuyerProfile />
-        <Login />
-        <MyOrder />
-        <Navbar />
-        <OrderDetail />
-        <OrderForm />
-        <Register />
-        <SupplierProfile />
+        <BrowserRouter>
+          <div>
+            <Navbar
+              authed={this.state.authed}
+              runAway={this.runAway}
+            />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Login}/>
+                  <PrivateRoute
+                    path="/myorder"
+                    authed={this.state.authed}
+                    component={MyOrder}
+                  />
+                  <PrivateRoute
+                    path="/supplierprofile"
+                    authed={this.state.authed}
+                    component={SupplierProfile}
+                  />
+                  <PrivateRoute
+                    path="/orderform"
+                    authed={this.state.authed}
+                    component={OrderForm}
+                  />
+                  <PrivateRoute
+                    path="/orderdetail"
+                    authed={this.state.authed}
+                    component={OrderDetail}
+                  />
+                  <PrivateRoute
+                    path="/buyerprofile"
+                    authed={this.state.authed}
+                    component={BuyerProfile}
+                  />
+                  <PublicRoute
+                    path="/login"
+                    authed={this.state.authed}
+                    component={Login}
+                  />
+                  <PublicRoute
+                    path="/register"
+                    authed={this.state.authed}
+                    component={Register}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
