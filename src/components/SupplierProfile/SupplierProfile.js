540432b1e7c9eb283c507fc5b98535abec1b26ab
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-17 20:30:07 -0500

    created supplierProfile form, and GET data to populate the form

--- src/components/SupplierProfile/SupplierProfile.js
+++ src/components/SupplierProfile/SupplierProfile.js
@@ -4,19 +4,13 @@ import supplierProfileRequests from '../../firebaseRequests/supplierProfile';
 
 class SupplierProfile extends React.Component {
   state = {
-    supplierProfile: {
-      name: 'abc',
-      address: '123 street',
-      city: 'nashville',
-      state: 'tn',
-      zip: '10000',
-    },
+    supplierProfiles: [],
   }
 
   componentDidMount () {
     supplierProfileRequests.getRequest()
-      .then((res) => {
-        console.error(res);
+      .then((supplierProfiles) => {
+        this.setState({ supplierProfiles });
       })
       .catch((err) => {
         console.error('Error with getting supplier data:',err);
@@ -24,9 +18,107 @@ class SupplierProfile extends React.Component {
   };
 
   render () {
+    const supplierProfileComponent = this.state.supplierProfiles.map((supplierProfile) => {
+      return (
+        <div id="supplier-profile-container" className="col-sm-6">
+          <form className="form-horizontal col-sm-12">
+            <div className="form-group">
+              <label htmlFor="supplier-company" className="col-sm-4 control-label">
+                Company:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.name}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="supplier-address" className="col-sm-4 control-label">
+                Address:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.address}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-3 col-sm-offset-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.city}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-2">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.state}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.zip}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="supplier-address" className="col-sm-4 control-label">
+                Phone Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.phoneNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="supplier-address" className="col-sm-4 control-label">
+                Fax Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={supplierProfile.faxNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="supplier-address" className="col-sm-4 control-label">
+                Date:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    });
+
     return (
       <div className="SupplierProfile">
-        <h2>SupplierProfile</h2>
+        {supplierProfileComponent}
       </div>
     );
   };
