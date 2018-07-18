7ca1596b8e54fcd8d20d5def2b22d8756b3f366c
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-18 18:49:37 -0500

    added basic layout and style for BuyerProfile component

--- src/components/BuyerProfile/BuyerProfile.js
+++ src/components/BuyerProfile/BuyerProfile.js
@@ -4,8 +4,203 @@ import './BuyerProfile.css';
 class BuyerProfile extends React.Component {
   render () {
     return (
-      <div className="BuyerProfile">
-        <h2>BuyerProfile</h2>
+      <div id="buyer-profile-container" className="col-sm-12">
+        <div className="col-sm-6 left-panel">
+          <h2 className="col-sm-4">Order By</h2>
+          <form className="form-horizontal col-sm-12">
+            <div className="form-group">
+              <label htmlFor="buyer-company" className="col-sm-4 control-label">
+                Company:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.name}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Address:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.address}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-3 col-sm-offset-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.city}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-2">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.state}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.zip}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Phone Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.phoneNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Fax Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.faxNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Agent:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={moment().format('YYYY-MM-DD h:m:s a')}
+                  disabled
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="col-sm-6 right-panel">
+          <h2 className="col-sm-4">Ship To</h2>
+          <div className="col-sm-6" id="ship-to-checkbox-container">
+            <input type="checkbox" id="ship-to-checkbox"/>
+            <label htmlFor="ship-to-checkbox">Same as above</label>
+          </div>
+          <form className="form-horizontal col-sm-12">
+            <div className="form-group">
+              <label htmlFor="buyer-company" className="col-sm-4 control-label">
+                Company:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.name}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Address:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.address}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-3 col-sm-offset-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.city}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-2">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.state}
+                  disabled
+                />
+              </div>
+              <div className="col-sm-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.zip}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Phone Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.phoneNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Fax Number:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={supplierProfile.faxNumber}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="buyer-address" className="col-sm-4 control-label">
+                Contact:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  // value={moment().format('YYYY-MM-DD h:m:s a')}
+                  disabled
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   };
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './OrderForm.css';
 import SupplierProfile from '../../components/SupplierProfile/SupplierProfile';
+import BuyerProfile from '../../components/BuyerProfile/BuyerProfile';
 
 class OrderForm extends React.Component {
   render () {
@@ -8,6 +9,7 @@ class OrderForm extends React.Component {
       <div className="OrderForm">
         <h2>OrderForm</h2>
         <SupplierProfile />
+        <BuyerProfile />
       </div>
     );
   };
