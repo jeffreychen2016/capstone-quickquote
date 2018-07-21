41b6ca41aa64e3bd2a4376ca4dafd5b21bfc9e22
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-21 10:49:44 -0500

    added OrderTable component, and created static table layout

---
+++ src/components/OrderTable/OrderTable.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './OrderTable.css';
+import {Table} from 'react-bootstrap';
+
+class OrderTable extends React.Component {
+  render () {
+    return (
+      <div className="OrderTable">
+        <h2>Order Form</h2>
+        <Table responsive id="order-table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Item</th>
+              <th>Description</th>
+              <th>Quantity</th>
+              <th>Unit Price</th>
+              <th>Amount</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>;
+      </div>
+    );
+  }
+}
+
+export default OrderTable;
