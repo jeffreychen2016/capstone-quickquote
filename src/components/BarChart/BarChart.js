4a52a6ba576782ad2e2cde95c4de6edf5336aa0f
Xuehui Chen <jeffreychen2016@gmail.com>
2018-08-01 20:47:23 -0500

    re-do the BarChart component. Originally got all data back from database, then filter them on client. Now, filter the data before return to the client

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -5,13 +5,13 @@ import orderRequests from '../../firebaseRequests/order';
 import itemRequests from '../../firebaseRequests/item';
 import orderItemRequests from '../../firebaseRequests/orderItem';
 import authRequests from '../../firebaseRequests/auth';
+import firebase, { firestore } from 'firebase';
 import moment from 'moment';
 
 class BarChart extends React.Component {
   state = {
     mergedData: [],
     input: '',
-    janData: [],
     data: {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
       datasets: [
@@ -30,153 +30,24 @@ class BarChart extends React.Component {
 
   componentDidMount () {
     const orderFlag = authRequests.getUserId() + '-' + '1';
-    orderRequests.getAllOrders(orderFlag)
-      .then((allSalesOrders) => {
-        allSalesOrders.map((order) => {
-          const year = moment(order.date).year();
-          const month = moment(order.date).month() + 1;
-          orderItemRequests.getAllOrderItemsForGivenOrderNumber(order.id)
-            .then((soitems) => {
-              soitems.map((soitem) => {
-                itemRequests.getAllItemsBasedOnItemId(soitem.itemid)
-                  .then((items) => {
-                    items.map((item) => {
-                      const mergedData = {
-                        year,
-                        month,
-                        item: item.code,
-                        quantity: soitem.quantity,
-                        price: item.price,
-                        total: (soitem.quantity * 1) * item.price,
-                      };
-                      this.state.mergedData.push(mergedData);
-                    });
-                  });
-              });
-            });
-        });
-      })
-      .catch((err) => {
-        console.error('Error getting all orders:', err);
-      });
+    this.getData();
+  };
+
+  getData = () => {
+    const rootRef = firebase.database().ref();
+    const soRef = rootRef.child('so').orderByChild('date').startAt('2018');
+    soRef.on('value',(snapshot) => {
+      console.error('so', snapshot.val());
+    });
   };
 
   getValue = (e) => {
     this.setState({input: e.target.value});
   };
 
-  renderDataInChart = (e) => {
-    if (e.key === 'Enter') {
-      const monthTotal = [];
-
-      const janData = [];
-      const febData = [];
-      const marData = [];
-      const aprData = [];
-      const mayData = [];
-      const junData = [];
-      const julData = [];
-      const augData = [];
-      const sepData = [];
-      const octData = [];
-      const novData = [];
-      const decData = [];
-
-      let janTotal = 0;
-      let febTotal = 0;
-      let marTotal = 0;
-      let aprTotal = 0;
-      let mayTotal = 0;
-      let junTotal = 0;
-      let julTotal = 0;
-      let augTotal = 0;
-      let sepTotal = 0;
-      let octTotal = 0;
-      let novTotal = 0;
-      let decTotal = 0;
-      this.state.mergedData.map((data) => {
-        if (data.year  === e.target.value * 1 && data.month === 1) {
-          janData.push(data.total);
-          janTotal = janData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 2) {
-          febData.push(data.total);
-          febTotal = febData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 3) {
-          marData.push(data.total);
-          marTotal = marData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 4) {
-          aprData.push(data.total);
-          aprTotal = aprData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 5) {
-          mayData.push(data.total);
-          mayTotal = mayData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 6) {
-          junData.push(data.total);
-          junTotal = junData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 7) {
-          julData.push(data.total);
-          julTotal = julData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 8) {
-          augData.push(data.total);
-          augTotal = augData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 9) {
-          sepData.push(data.total);
-          sepTotal = sepData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 10) {
-          octData.push(data.total);
-          octTotal = octData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 11) {
-          novData.push(data.total);
-          novTotal = novData.reduce((a,b) => {
-            return a + b;
-          });
-        } else if (data.year === e.target.value * 1 && data.month === 12) {
-          decData.push(data.total);
-          decTotal = decData.reduce((a,b) => {
-            return a + b;
-          });
-        };
-      });
-      monthTotal.push(janTotal);
-      monthTotal.push(febTotal);
-      monthTotal.push(marTotal);
-      monthTotal.push(aprTotal);
-      monthTotal.push(mayTotal);
-      monthTotal.push(junTotal);
-      monthTotal.push(julTotal);
-      monthTotal.push(augTotal);
-      monthTotal.push(sepTotal);
-      monthTotal.push(octTotal);
-      monthTotal.push(novTotal);
-      monthTotal.push(decTotal);
-
-      const tempDatasets = [...this.state.data.datasets];
-      const tempData = {...this.state.data};
-      tempDatasets[0].data = monthTotal;
-      tempData.datasets = tempDatasets;
-      this.setState({data: tempData});
-    };
-  };
+  renderDataInChart = () => {
+    this.getData();
+  }
 
   render () {
     return (
