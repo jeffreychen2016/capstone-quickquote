9e90e0e59bde473dc4bda1ac0898b73504ff5ddd
Xuehui Chen <jeffreychen2016@gmail.com>
2018-08-03 20:38:14 -0500

    created Map component files for creating Map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Map.css';
+
+class Map extends React.Component {
+  render () {
+    return (
+      <div className="Map">
+        <h2>Map</h2>
+      </div>
+    );
+  }
+}
+
+export default Map;
