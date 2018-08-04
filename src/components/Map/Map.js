726a70a7303b35126d0522b1210e68593b793a1b
Xuehui Chen <jeffreychen2016@gmail.com>
2018-08-03 20:55:41 -0500

    created getCoordinate function to find coordinates with address for using google map later

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Map.css';
-import Geocode from "react-geocode";
+import Geocode from 'react-geocode';
 
 class Map extends React.Component {
+
+  componentDidMount () {
+    this.getCoordinate();
+  };
+
+  getCoordinate = () => {
+    Geocode.fromAddress('320 swynford ct').then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.error(lat, lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  };
+
   render () {
     return (
       <div className="Map col-sm-6">
